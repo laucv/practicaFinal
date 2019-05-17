388d5e7f74d813eb31ff519ad5908c5cf5bc0258
laucv <laura.canelada.velasco@alumnos.upm.es>
2019-05-17 18:11:16 +0200

    implementado GET cuestiones y DELETE cuestiones con fallos (se cierra el servidor)

--- public/frontEnd/js/cuestionesMaestro.js
+++ public/frontEnd/js/cuestionesMaestro.js
@@ -2,7 +2,7 @@ function imprimirCuestion(cuestion) {
     'use strict';
     var lista = document.getElementById("listaCuestiones");
     var elementoLista = document.createElement("li");
-    elementoLista.setAttribute("id", "cuestion-" + cuestion.clave);
+    elementoLista.setAttribute("id", "cuestion-" + cuestion.cuestion.idCuestion);
     lista.appendChild(elementoLista);
 
     var div = document.createElement("div");
@@ -10,7 +10,7 @@ function imprimirCuestion(cuestion) {
     elementoLista.appendChild(div);
 
     var abrir = document.createElement("a");
-    abrir.setAttribute("href", "modificarPregunta.html?clave=" +cuestion.clave);
+    abrir.setAttribute("href", "modificarPregunta.html?idCuestion=" +cuestion.cuestion.idCuestion);
     abrir.setAttribute("class", "btn btn-info");
     abrir.setAttribute("role", "button");
     abrir.innerHTML = "Abrir pregunta";
@@ -19,43 +19,100 @@ function imprimirCuestion(cuestion) {
     var eliminar = document.createElement("button");
     eliminar.setAttribute("class", "btn btn-danger");
     eliminar.setAttribute("type", "button");
-    eliminar.setAttribute("onclick", "deleteQuestion(" + cuestion.clave + ");");
+    eliminar.setAttribute("onclick", "deleteQuestion(" + cuestion.cuestion.idCuestion + ");");
     eliminar.innerHTML = "Eliminar pregunta";
     div.appendChild(eliminar);
 
     var pregunta = document.createElement("p");
-    pregunta.setAttribute("id", "pregunta-" + cuestion.clave);
+    pregunta.setAttribute("id", "pregunta-" + cuestion.cuestion.idCuestion);
     elementoLista.appendChild(pregunta);
 
 
     var enlace = document.createElement("a");
-    enlace.setAttribute("href", "abrirPregunta.html?clave=" +cuestion.clave);
-    enlace.innerHTML = cuestion.pregunta;
+    enlace.setAttribute("href", "abrirPregunta.html?idCuestion=" +cuestion.cuestion.idCuestion);
+    enlace.innerHTML = cuestion.cuestion.enunciadoDescripcion;
     pregunta.appendChild(enlace);
 }
 
 function listarPreguntas() {
     'use strict';
-    var datos = JSON.parse(window.localStorage.getItem("datos"));
+    var datos = JSON.parse(window.sessionStorage.getItem("cuestiones"));
     var cuestion;
     for (cuestion of datos.cuestiones) {
         imprimirCuestion(cuestion);
     }
 }
 
-function deleteQuestion(clave){
-  var datos = JSON.parse(window.localStorage.getItem("datos"));
-
-  for( var i = 0; i < datos.cuestiones.length; i++){ 
-    
-   if ( datos.cuestiones[i].clave === clave) {
-     datos.cuestiones.splice(i, 1); 
-     for (var j=i; j<datos.cuestiones.length; j++){
-       datos.cuestiones[j].clave = datos.cuestiones[j].clave - 1;
-     }
-   }
-}  
-  window.localStorage.setItem("datos", JSON.stringify(datos));
-  location.reload();
+function deleteQuestion(idCuestion){
+    function trataRespuesta () {
+        var respuesta;
+        if(request.status==204){
+            respuesta = request.response;
+            let string_cuestion = JSON.stringify(respuesta);
+            let cuestion = JSON.parse(string_cuestion);
+            location.reload();
+        }
+        else{
+            alert("No se ha encontrado la cuestion a eliminar");
+        }
+    }
+    var request = new XMLHttpRequest();
+    let token = window.sessionStorage.getItem("token");
+    let url = 'http://localhost:8000/api/v1/questions/' + idCuestion;
+    request.open('DELETE', url, true);
+    request.setRequestHeader("Authorization", "Bearer " + token);
+    request.responseType = 'json';
+    request.onload = trataRespuesta;
+    request.send();
 
 }
+
+function getCuestiones() {
+    'use strict';
+    function trataRespuesta () {
+        var respuesta;
+        if(request.status==200){
+            respuesta = request.response;
+            let string_cuestiones = JSON.stringify(respuesta);
+            let cuestiones = JSON.parse(string_cuestiones);
+            window.sessionStorage.setItem("cuestiones", JSON.stringify(cuestiones));
+            listarPreguntas();
+        }
+        else{
+            alert("No se han encontrado cuestiones");
+        }
+    }
+    var request = new XMLHttpRequest();
+    let token = window.sessionStorage.getItem("token");
+    request.open('GET', 'http://localhost:8000/api/v1/questions', true);
+    request.setRequestHeader("Authorization", "Bearer " + token);
+    request.responseType = 'json';
+    request.onload = trataRespuesta;
+    request.send();
+
+}
+/*
+function getCuestion(idCuestion) {
+    'use strict';
+    function trataRespuesta () {
+        var respuesta;
+        if(request.status==200){
+            respuesta = request.response;
+            let string_cuestion = JSON.stringify(respuesta);
+            let cuestion = JSON.parse(string_cuestion);
+            window.sessionStorage.setItem("cuestion", JSON.stringify(cuestion));
+        }
+        else{
+            alert("No existe la cuestion");
+        }
+    }
+    var request = new XMLHttpRequest();
+    let token = window.sessionStorage.getItem("token");
+    let url = 'http://localhost:8000/api/v1/questions/' + idCuestion;
+    request.open('GET', url, true);
+    request.setRequestHeader("Authorization", "Bearer " + token);
+    request.responseType = 'json';
+    request.onload = trataRespuesta;
+    request.send();
+}
+*/
